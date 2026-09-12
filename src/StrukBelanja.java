f8b8604a0989098db8926cc076594b4d20e33877
Nathaniel Adelio <Nathan@unai.edu>
2026-09-12 22:14:20 +0700

    tambahin nama barang jadi 3 sama harga satuan

--- src/StrukBelanja.java
+++ src/StrukBelanja.java
@@ -16,6 +16,21 @@ public class StrukBelanja {
         System.out.print("Harga satuan barang 3: ");
         int harga3 = input.nextInt();
 
+        System.out.print("Jumlah barang 3: ");
+        int jumlah3 = input.nextInt();
+
+        System.out.print("Harga satuan barang 1: ");
+        int harga1 = input.nextInt();
+
+        System.out.print("Jumlah barang 1: ");
+        int jumlah1 = input.nextInt();
+
+        System.out.print("Harga satuan barang 2: ");
+        int harga2 = input.nextInt();
+
+        System.out.print("Jumlah barang 2: ");
+        int jumlah2 = input.nextInt();
+
         input.close();
     }
 }

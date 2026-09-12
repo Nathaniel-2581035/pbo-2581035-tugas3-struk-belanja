8285348e2f572496335d242b2a23bd3144ac93ac
Nathaniel Adelio <Nathan@unai.edu>
2026-09-12 19:50:00 +0700

    perbaikan nama barang dan penambahan satu harga barang

--- src/StrukBelanja.java
+++ src/StrukBelanja.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class StrukBelanja {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Nama Barang: ");
-        String
+        System.out.print("Nama barang 1: ");
+        String nama1 = input.nextLine();
+
+        System.out.print("Nama barang 2: ");
+        String nama2 = input.nextLine();
+
+        System.out.print("Nama barang 3: ");
+        String nama3 = input.nextLine();
+
+        System.out.print("Harga satuan barang 3: ");
+        int harga3 = input.nextInt();
+
+        input.close();
     }
 }

f98775f5d2634a0022224266400f7fcb46fd385a
Nathaniel Adelio <Nathan@unai.edu>
2026-09-12 19:41:03 +0700

    menambhakan harga satuan

---
+++ src/StrukBelanja.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StrukBelanja {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Nama Barang: ");
+        String
+    }
+}

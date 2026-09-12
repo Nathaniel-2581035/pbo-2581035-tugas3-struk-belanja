879c763841c8edd198779d8ad46fbe218e9bfe31
Nathaniel Adelio <Nathan@unai.edu>
2026-09-12 22:26:34 +0700

    perbaikin urutan yang salah

--- src/StrukBelanja.java
+++ src/StrukBelanja.java
@@ -6,30 +6,27 @@ public class StrukBelanja {
 
         System.out.print("Nama barang 1: ");
         String nama1 = input.nextLine();
+        System.out.print("Harga satuan barang 1: ");
+        int harga1 = input.nextInt();
+        System.out.print("Jumlah barang 1: ");
+        int jumlah1 = input.nextInt();
+        input.nextLine();
 
         System.out.print("Nama barang 2: ");
         String nama2 = input.nextLine();
+        System.out.print("Harga satuan barang 2: ");
+        int harga2 = input.nextInt();
+        System.out.print("Jumlah barang 2: ");
+        int jumlah2 = input.nextInt();
+        input.nextLine();
 
         System.out.print("Nama barang 3: ");
         String nama3 = input.nextLine();
-
         System.out.print("Harga satuan barang 3: ");
         int harga3 = input.nextInt();
-
         System.out.print("Jumlah barang 3: ");
         int jumlah3 = input.nextInt();
-
-        System.out.print("Harga satuan barang 1: ");
-        int harga1 = input.nextInt();
-
-        System.out.print("Jumlah barang 1: ");
-        int jumlah1 = input.nextInt();
-
-        System.out.print("Harga satuan barang 2: ");
-        int harga2 = input.nextInt();
-
-        System.out.print("Jumlah barang 2: ");
-        int jumlah2 = input.nextInt();
+        input.nextLine();
 
         System.out.print("Uang dibayar: ");
         int uangDibayar = input.nextInt();
